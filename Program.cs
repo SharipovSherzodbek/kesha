671fc7e118dc5729515aa2757d2b101f60da962f
Sherzodbek Sharipov <sherzodbekdeveloper@gmail.com>
2024-01-14 17:27:53 +0500

    Arrey

--- Program.cs
+++ Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+string parol = "";
+
+do
+{
+    System.Console.WriteLine("Enter password to run \"123\"");
+    parol = Console.ReadLine();
+}
+while (parol != "123");
 
 Console.WriteLine("CONGRATULATIONS !, Now you have a chanece to win 1ooo$");
 // Console.WriteLine("To get a gift \"You have to SIGN IN and answer for 5 questions ready?\"");
@@ -13,7 +21,6 @@ Console.WriteLine("CONGRATULATIONS !, Now you have a chanece to win 1ooo$");
 Console.WriteLine("Enter your Full name:");
 // Console.WriteLine("Caution! be carefull in entering your name and email to win!");
 string name = Console.ReadLine();
-Console.WriteLine("Loading...");
 Console.WriteLine($"Hello {name} ");
 // Console.WriteLine("To find you, We must know your phone number! Please Enter:");
 // string phoneNumber = Console.ReadLine();
@@ -28,12 +35,11 @@ Console.WriteLine("Loading...");
 // Console.WriteLine("In order to quit click \"0\" to continue click \"1\"");
 // string button = Console.ReadLine();
 // Console.WriteLine($"You entered {button}");
-// Console.WriteLine("Loading....");
 // Console.WriteLine("It was a my first project on C# to make you a little bit happy");
-// Console.WriteLine("Main idea of this program to find age difference between us");
+Console.ReadLine();
+Console.WriteLine("Main idea of this program to find age difference between us");
 int myAge = 27;
 // Console.WriteLine("To know age Difference click \"Yes\" to quit \"No\"");
-// Console.WriteLine("Loading...");
 // string press = System.Console.ReadLine();
 // System.Console.WriteLine($"You entered {press}");
 int ageDifference = myAge-yourAge;
@@ -67,10 +73,28 @@ else
 }
 
 // <  <=  >  >=  ==  !=
-// Console.WriteLine($"You are younger than me {myAge>yourAge}");
-// Console.WriteLine($"You are older than me {myAge<yourAge}");
-// Console.WriteLine($"You are older or equal me {myAge<=yourAge}");
-// Console.WriteLine($"You are younger or equal {myAge>=yourAge}");
-// Console.WriteLine($"Your age is equal to mine {myAge==yourAge}");
-// Console.WriteLine($"Your age is not equal to mine {myAge!=yourAge}");
-// System.Console.WriteLine($"Your age is less than 10 {yourAge<10}");
+
+System.Console.ReadLine();
+System.Console.WriteLine("Let me introduce you my friends!");
+
+//string [] friendsName = { "Vasya", "Ilya", "Donka"}; yoki
+string [] friendsName = new string [3]; 
+friendsName[0] = "Vasya";
+friendsName[1] = "Ilya";
+friendsName[2] = name;
+int [] friendsAge = {45, 14, yourAge };
+string[] address = {"Chust", "Samarqand", "Tashkent"};
+System.Console.WriteLine($"{friendsName[0]} is {friendsAge[1]} years old" );
+System.Console.WriteLine($"{friendsName[2]} is {friendsAge[2]} years old" );
+System.Console.WriteLine($"He is from {address[1]}");
+
+friendsAge[2] = int.Parse(Console.ReadLine());
+System.Console.WriteLine($"{friendsName[1]} is {friendsAge[2]} years old" );
+
+//FOR va FOR EACH
+foreach (string ism in friendsName)
+{
+    System.Console.WriteLine(ism);
+}
+
+
